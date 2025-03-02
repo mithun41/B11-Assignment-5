8d12cd1e48a2947b5c773620b4cf76ec74beec43
mithun8441 <mahmudulhasan3527@gmail.com>
2025-03-02 12:24:57 +0600

    added dynamic date

--- script/clear.js
+++ script/clear.js
@@ -4,7 +4,34 @@ clearH.addEventListener("click", function () {
   createHistory.innerHTML = "";
 });
 
+// Go to Blog Page
 const goback = document.getElementById("blog");
 goback.addEventListener("click", function () {
   window.location.href = "./blog.html";
 });
+
+// Dynamic Date set
+
+const currentDate1 = document.getElementById("date").innerText;
+const now = new Date();
+const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const dayOfWeek = daysOfWeek[now.getDay()];
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+const month = months[now.getMonth()];
+const dayOfMonth = now.getDate();
+const year = now.getFullYear();
+const dateString = `${dayOfWeek}, ${month} ${dayOfMonth} ${year}`;
+document.getElementById("date").innerText = dateString;
